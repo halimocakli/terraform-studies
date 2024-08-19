da2b5f20dd6fd023c330a78fa9288720994652f1
halimocakli <halimcanocakli@gmail.com>
2024-08-19 22:26:12 +0300

    Feat: Terraform Maps examined.

---
+++ 27-Input-Variables-[Collection-Type]-Maps/terraform-manifests/c1-versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "myRandomString" {
+  length  = 6
+  upper   = false
+  special = false
+  numeric = false
+}
---
+++ 27-Input-Variables-[Collection-Type]-Maps/terraform-manifests/c2-variables.tf
@@ -0,0 +1,65 @@
+# Input Variables
+
+# 1. Business Unit Name
+variable "business_unit" {
+  description = "Business Unit Name"
+  type        = string
+  default     = "hr"
+}
+# 2. Environment Name
+variable "environment" {
+  description = "Environment Name"
+  type        = string
+  default     = "dev"
+}
+# 3. Resource Group Name
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+  default     = "myrg"
+}
+# 4. Resource Group Location
+variable "resource_group_location" {
+  description = "Resource Group Location"
+  type        = string
+  default     = "East US"
+}
+# 5. Virtual Network Name
+variable "virtual_network_name" {
+  description = "Virtual Network Name"
+  type        = string
+  default     = "myvnet"
+}
+# 6. Subnet Adı: Bu değişkenin değeri, "promt" aşamasında CLI üzerinden atanacaktır.
+variable "subnet_name" {
+  description = "Virtual Network Subnet Name"
+  type        = string
+  default     = "mysubnet"
+}
+# 7. Virtual Network address_space
+# Burada oluşturduğumuz değişken list tipindedir.
+# Bu değişkenin değeri, "terraform.tfvars" dosyasında atanacaktır.
+# Değer atanmaması durumunda, aşağıda varsayılan olarak belirlenen değerler kullanılacaktır.
+variable "virtual_network_address_space" {
+  description = "Virtual Network Address Space"
+  type        = list(string)
+  default     = ["10.0.0.0/16", "10.1.0.0/16", "10.2.0.0/16"]
+}
+# 8. Public IP SKU
+variable "public_ip_sku" {
+  description = "Azure Public IP SKU"
+  type        = map(string)
+  default = {
+    "eastus"  = "Basic"
+    "eastus2" = "Standard"
+  }
+}
+# 9. Common Tags
+variable "common_tags" {
+  description = "Common Tags for Azure Resources"
+  type        = map(string)
+  default = {
+    "CLI_Tool" = "Terraform"
+    "Tag_1"    = "Azure"
+  }
+}
